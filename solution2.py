6720c221acdd60956edb4e2ca09390effade4147
shailendrak16 <67014815+shailendrak16@users.noreply.github.com>
2020-08-22 23:12:53 +0800

    Add files via upload

---
+++ solution2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 14:32:18 2020
+
+@author: admin
+"""
+
+import pandas as pd
+from itertools import combinations 
+  
+def getpair(mylist, k): 
+    return [(i, j) for i, j in combinations(mylist, r = 2) 
+                   if abs(i - j) == k] 
+              
+# Driver code 
+lst = [1,3,5] 
+k = 2
+outpairs = getpair(lst,k)
+
+print("we will have",len(outpairs),"pairs:", outpairs) 
